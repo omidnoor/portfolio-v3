140fa5d3b7b74fda066262d95ad07395c95e8625
Omid Noorshams <o.noorshams@gmail.com>
2023-06-05 01:02:51 -0700

    remoce CustomMesh and add it to ImageFrame. add 5 frames to the scene

--- src/components/3d/ImageFrame.jsx
+++ src/components/3d/ImageFrame.jsx
@@ -1,109 +1,103 @@
 import Home from "@/components/pageComponents/Home";
 import { useStore } from "@/stores/store";
-import { Html, Image, Text, useCursor } from "@react-three/drei";
+import { Html, useCursor } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { damp3, dampC } from "maath/easing";
+import { damp3, dampC, dampQ } from "maath/easing";
 import { useRef, useState } from "react";
 import { Color } from "three";
-import getUuidByString from "uuid-by-string";
-import { useRoute } from "wouter";
-import AboutMe from "../pageComponents/AboutMe";
+
 import { CustomMesh } from "./CustomMesh";
+import { useEffect } from "react";
 
 const GOLDENRATIO = 1.61803398875;
+
+const outerScale = [1, GOLDENRATIO, 0.05];
+const outerPosition = [0, GOLDENRATIO / 2, 0];
+const innerScale = [0.9, 0.93, 0.9];
+const innerPosition = [0, 0, 0.2];
 
 const ImageFrame = ({
   c = new Color(),
   portal,
-  hovered,
-  setHovered,
   setClicked,
-  handleClick,
+  targetPosition,
+  targetQuaternion,
   ...props
 }) => {
-  const nameRef = useRef();
-  const imageRef = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [nameUuid, setNameUuid] = useState(name);
   const frameRef = useRef();
 
-  // const activePage = useStore((state) => state.pages[pageName]);
-  const activeFrame = useStore((state) => state.activeFrame);
   const setActiveFrame = useStore((state) => state.setActiveFrame);
+  const setFrameEventName = useStore((state) => state.setFrameEventName);
+  const activeFrame = useStore((state) => state.activeFrame);
 
-  const [rnd] = useState(() => Math.random());
-
-  // const name = getUuidByString(url);
-
+  useEffect(() => {
+    setNameUuid(frameRef.current?.uuid);
+  }, []);
   useFrame((state, delta) => {
-    // imageRef.current.material.zoom =
-    //   1 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) * 0.1;
-    // damp3(
-    //   imageRef.current.scale,
-    //   [0.85 * (hovered ? 0.85 : 1), 0.9 * (hovered ? 0.905 : 1), 1],
-    //   0.1,
-    //   delta,
-    // );
-    if (frameRef.current) {
-      dampC(
-        frameRef.current.material.color,
-        hovered ? [1, 0.647, 0] : [1, 1, 1],
-        0.1,
-        delta,
-      );
-    }
+    if (!frameRef.current) return;
+    // console.log(frameRef.current);
+    dampC(
+      frameRef.current?.material?.color,
+      hovered ? [1, 0.647, 0] : [1, 1, 1],
+      0.1,
+      delta,
+    );
   });
 
+  const handleClick = (event) => {
+    // event.stopPropagation();
+    setClicked(true);
+    if (event.object) {
+      const frameName = event.object.name;
+      setActiveFrame(frameName);
+      setFrameEventName(frameName);
+    }
+  };
+
+  useCursor(hovered);
+
   return (
-    <group>
-      {/* <mesh
+    <group
+      // ref={frameRef}
+      onPointerOver={(event) => {
+        setFrameEventName(event.object.name);
+        setHovered(true);
+      }}
+      onPointerOut={(e) => {
+        setHovered(false);
+      }}
+      onPointerMissed={() => setActiveFrame(null)}
+    >
+      <mesh
         onClick={handleClick}
-        scale={[1, GOLDENRATIO, 0.05]}
-        position={[0, GOLDENRATIO / 2, 0]}
-        name={frameRef.current?.uuid}
+        scale={outerScale}
+        position={outerPosition}
+        name={nameUuid}
+        {...props}
       >
         <boxGeometry />
         <meshStandardMaterial
-          color="#00007f"
+          color="#151515"
           metalness={0.5}
           roughness={0.5}
           envMapIntensity={2}
         />
-
         <mesh
           ref={frameRef}
           raycast={() => null}
-          scale={[0.9, 0.93, 0.9]}
-          position={[0, 0, 0.2]}
+          scale={innerScale}
+          position={innerPosition}
         >
           <boxGeometry />
-          <meshBasicMaterial toneMapped={false} fog={false} /> */}
-      <CustomMesh frameRef={frameRef} handleClick={handleClick} {...props}>
-        <Html className="content-embed" portal={portal} scale={0.1} transform>
-          <Home onHover={setHovered} setClicked={setClicked} />
-        </Html>
-      </CustomMesh>
-      {/* </mesh>
-      </mesh> */}
+          <meshBasicMaterial fog={false} toneMapped={false} />
+          <Html className="content-embed" portal={portal} scale={0.1} transform>
+            <Home onHover={setHovered} setClicked={setClicked} />
+          </Html>
+        </mesh>
+      </mesh>
     </group>
   );
 };
 export default ImageFrame;
-
-{
-  /* <Image
-          raycast={() => null}
-          ref={imageRef}
-          position={[0, 0, 0.7]}
-          url={url}
-        /> */
-}
-{
-  /* <Text
-          maxWidth={0.1}
-          anchorX="left"
-          anchorY="top"
-          position={[0, GOLDENRATIO * 0.4, 0]}
-          fontSize={0.025}
-        >
-          {name.split("-").join(" ")}
-        </Text> */
-}
--- src/components/3d/ImageFrames.jsx
+++ src/components/3d/ImageFrames.jsx
@@ -14,27 +14,20 @@ const ImageFrames = ({
   targetPosition = new Vector3(),
   targetQuaternion = new Quaternion(),
 }) => {
-  const framesRef = useRef(null);
-  const clickedRef = useRef(null);
-
   const [clicked, setClicked] = useState(false);
-  const [hovered, setHovered] = useState(false);
   const [frameEventName, _] = useState(null);
-
-  useCursor(hovered);
+  const framesRef = useRef(null);
 
   const activeFrame = useStore((state) => state.activeFrame);
   const setActiveFrame = useStore((state) => state.setActiveFrame);
   const setFrameEventName = useStore((state) => state.setFrameEventName);
 
-  useCursor(portal);
-
   useEffect(() => {
     if (activeFrame) {
       const frame = framesRef.current.getObjectByName(activeFrame);
-      frame.parent.updateWorldMatrix(true, true);
-      frame.parent.localToWorld(targetPosition.set(0, GOLDENRATIO * 0.5, 1.25));
-      frame.parent.getWorldQuaternion(targetQuaternion);
+      frame.updateWorldMatrix(true, true);
+      frame.localToWorld(targetPosition.set(0, GOLDENRATIO * 0, 20));
+      frame.getWorldQuaternion(targetQuaternion);
     } else {
       targetPosition.set(0, 0, 5.5);
       targetQuaternion.identity();
@@ -46,39 +39,27 @@ const ImageFrames = ({
     dampQ(state.camera.quaternion, targetQuaternion, 0.4, delta);
   });
 
-  const handleClick = (event) => {
-    // event.stopPropagation();
-    setClicked(true);
-    if (event.object) {
-      const frameName = event.object.name;
-      setActiveFrame(frameName);
-      setFrameEventName(frameName);
-    }
-  };
+  // const handleClick = (event) => {
+  //   // event.stopPropagation();
+  //   setClicked(true);
+  //   if (event.object) {
+  //     const frameName = event.object.name;
+  //     setActiveFrame(frameName);
+  //     setFrameEventName(frameName);
+  //   }
+  // };
 
   return (
-    <group
-      ref={framesRef}
-      // onClick={handleClick}
-      onPointerOver={(event) => {
-        setFrameEventName(event.object.name);
-        setHovered(true);
-      }}
-      onPointerOut={(e) => {
-        setHovered(false);
-      }}
-      onPointerMissed={() => setActiveFrame(null)}
-    >
+    <group ref={framesRef}>
       {pages?.map((props, index) => (
         <ImageFrame
           key={index}
-          hovered={hovered}
-          setHovered={setHovered}
           portal={portal}
-          framesRef={framesRef}
           setClicked={setClicked}
           frameEventName={frameEventName}
-          handleClick={handleClick}
+          targetPosition={targetPosition}
+          targetQuaternion={targetQuaternion}
+          // handleClick={handleClick}
           {...props}
         />
       ))}
